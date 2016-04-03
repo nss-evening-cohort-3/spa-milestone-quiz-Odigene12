3c467e2e3f2d80ae3b0c4aeff8a0a2d97f35b736
Odie Joseph <Odie Joseph>
2016-04-02 22:39:44 -0500

    My code will not populate to the dom. please help

--- quiz.js
+++ quiz.js
@@ -1,20 +1,21 @@
-"use script"
-
-function populatePage (inventory) {
+"use strict"
+let CarLot = (function (originalCarLot) {
+	let container = document.getElementById("container");
+	let vehicles = cars[i];
+function populatePage (originalCarLot) {
   // Loop over the inventory and populate the page
-  	for (var i = 0; i < cars.length; i++) {
-  		let theCar = cars[i];
-  			buildCard(theCar); 
+  	for(var i = 0; i < cars.length; i++) {
+originalCarLot.carstoDOM = function (vehicles) {
+  		container.innerHTML += `<h1 class = "type">${vehicles.make} ${vehicles.model}</h1><section><p>${vehicles.year} ${vehicles.price}</p><footer>${vehicles.purchased} ${vehicles.description}</footer>`
   	};
-// Now that the DOM is loaded, establish all the event listeners needed
-  CarLot.activateEvents();
-}
+ }
 
+  	// build each car card
+  	
+  // Now that the DOM is loaded, establish all the event listeners needed
+  CarLot.activateEvents();
+	}
+}
 // Load the inventory and send a callback function to be
 // invoked after the process is complete
 CarLot.loadInventory();
-
-// create a function that takes the information from json and builds card for each car
-function buildCard(theCar) {
-	container.innerHTML += `<theCar class = "card"><header>${theCar.make} ${theCar.model} ${theCar.year}</header><section><p>${theCar.price} ${theCar.color} ${theCar.purchased} ${theCar.description}</p>`
-}
