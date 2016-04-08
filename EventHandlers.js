ac1de2bd094b586a48817f4c49050a8d21313fc9
Odie Joseph <Odie Joseph>
2016-04-08 12:14:46 -0500

    fixed the problem with the binding. Bootstrap would mess up my CSS

--- EventHandlers.js
+++ EventHandlers.js
@@ -8,6 +8,8 @@ let fourthCar = document.getElementById("car3");
 let cars = document.getElementsByClassName("car");
 let input = document.getElementById("userInput");
 let carBio = document.getElementsByClassName("description");
+let chosenCar;
+let chosenBio;
 
 function activateEvents (inventory) {		
 
@@ -15,48 +17,50 @@ for(var i = 0; i < cars.length; i++) {
 	cars[i].addEventListener("click", function (event) {
 	originalCarLot.colorChange(event);
 	originalCarLot.borderChange(event);
-	originalCarLot.clearValue(event);
 	originalCarLot.changeBio();
+	
 		});
 	}
+	originalCarLot.bindItUp();
 };
-
-
-		originalCarLot.changeBio = function () {
-				let theseCars = document.getElementsByClassName("car");
-				let carBio = document.getElementsByClassName("description");
-				for (let i = 0; i < cars.length; i++) {
-					let newCar = theseCars[i];
-					let newDesc = carBio[i];
-			input.addEventListener("keyup", function(event) {
-				if (theseCars.classList.contains("selected")) {
-					let userWrite = event.currentTarget.value;
-					newDesc.innerHTML = userWrite;
-				}
-			})	
+	originalCarLot.changeBio = function() {
+		let demCars = document.getElementsByClassName("car");
+		let carBio = document.getElementsByClassName("bio");
+		for(let i = 0; i < cars.length; i++) {
+			let thisCar = demCars[i];
+			let thisBio = carBio[i];
+			thisCar.addEventListener("click", function() {
+				CarLot.removeSelection();
+				thisCar.classList.add("selected")
+				input.value = "";
+				input.focus();	
+				chosenCar = thisCar;
+				chosenBio = thisBio;						
+				
+			})
 		}
-	}	
-		originalCarLot.removeSelection = function () {
-			let theseCars = document.getElementsByClassName("car");
-			for (let i = 0; i < cars.length; i++) {
-				cars[i].classList.remove("selected")
-			}
+	}
+
+
+	originalCarLot.removeSelection = function () {
+		let cars = document.getElementsByClassName("car");
+		for(let i = 0; i < cars.length; i++) {
+			cars[i].classList.remove("selected");
 		}
-
-
-
-
-	originalCarLot.clearValue = function(event){
-		for (let i = 0; i < cars.length; i++){
-			CarLot.removeSelection();
-			let thisCar = cars[i];
-			let thisDesc = carBio[i];
-			thisCar.classList.add("selected");
-			input.value= "";
-			input.focus();
-		}
-	}	
+	}
 		
+	originalCarLot.bindItUp = function() {
+		let theseCars = document.getElementsByClassName("car");
+		let theseBios = document.getElementsByClassName("description");
+			input.addEventListener("keypress", function(event) {
+				console.log(chosenCar);
+				if (chosenCar.classList.contains("selected")) {
+					let userDescription = event.currentTarget.value;
+					chosenBio.innerHTML = userDescription;
+				}
+			})
+		}
+
 	
 	originalCarLot.colorChange = function(event) {
 		if(event.target.parentNode.id === firstCar.id || event.target.id === firstCar.id ){
