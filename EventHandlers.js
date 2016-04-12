63b42c7a19cb51fedb8e34ff0eedce0d12001216
Odie Joseph <Odie Joseph>
2016-04-12 10:50:00 -0500

    Need to bind input text to car descriptions

--- EventHandlers.js
+++ EventHandlers.js
@@ -1,66 +1,40 @@
 "use strict"
 
 // This is the beginning of the iife in which the variables are set at the top. 
-var CarLot = ( function (originalCarLot) {
-let firstCar = document.getElementById("car0");	
-let secondCar = document.getElementById("car1");	
-let thirdCar = document.getElementById("car2");	
-let fourthCar = document.getElementById("car3");	
+var CarLot = ( function (originalCarLot) {	
 let cars = document.getElementsByClassName("car");
 let input = document.getElementById("userInput");
-let carBio = document.getElementsByClassName("description");
+let carBio = document.getElementsByClassName("bio");
+let carDes = document.getElementsByClassName("description");
 let chosenCar;
 let chosenBio;
 
 // The events are all called in this function.
 function activateEvents (inventory) {		
-
 for(var i = 0; i < cars.length; i++) {
-	cars[i].addEventListener("click", function (event) {
-	originalCarLot.changeBio();
-	
+		let chosenBio = carBio[i]
+		let chosenCar = cars[i];
+	cars[i].addEventListener("click", function (event) {	
+	originalCarLot.removeSelection("colorBack", "borderBack")	
+	originalCarLot.colorChange(event.currentTarget, "colorBack");
+	originalCarLot.borderChange(event.currentTarget, "borderBack");
+	originalCarLot.bindItUp();
 		});
 	}
-	originalCarLot.bindItUp();
+	
 };
 
-	// This function is to go through each car on the dom and attach one of the css classes to them when clicked
-	originalCarLot.changeBio = function() {
-		let demCars = document.getElementsByClassName("car");
-		let carBio = document.getElementsByClassName("bio");
-		for(let i = 0; i < cars.length; i++) {
-			let thisCar = demCars[i];
-			let thisBio = carBio[i];
-			thisCar.addEventListener("click", function() {
-				CarLot.removeSelection();
-				thisCar.classList.add("selected")	
-				chosenCar = thisCar;
-				chosenBio = thisBio;
-				if (event.target.id === car[i])						
-				
-			})
-		}
-	}
 
 	// This function is supposed to remove deselect the car that was previously clicked on when another car is clicked.
-	originalCarLot.removeSelection = function () {
-		let cars = document.getElementsByClassName("car");
-		for(let i = 0; i < cars.length; i++) {
-			cars[i].classList.remove("selected");
+	originalCarLot.removeSelection = function (ClassName, BorderName) {
+		for(var i = 0; i < cars.length; i++){
+			cars[i].classList.remove(ClassName, BorderName)
 		}
 	}
 		//This function will allow the user to change the description for the selected car 
 	originalCarLot.bindItUp = function() {
-		let demCars = document.getElementsByClassName("car");
-		let carBio = document.getElementsByClassName("bio");
 			input.addEventListener("keypress", function(event) {
-				for(let i = 0; i < cars.length; i++){
-				let thisCar = demCars[i];
-				let thisBio = carBio[i];
-					chosenCar = thisCar;
-					chosenBio = thisBio;
-					console.log(chosenCar);
-				}	
+				
 			})
 		}
 // The iife is called and immediately loads.
@@ -68,4 +42,4 @@ CarLot.loadInventory(activateEvents);
 
 return originalCarLot;
 
-})(CarLot);
+}) (CarLot);
--- borderEvents.js
+++ borderEvents.js
@@ -13,41 +13,23 @@ for(var i = 0; i < cars.length; i++) {
 	cars[i].addEventListener("click", function (event) {
 				input.value = "";
 				input.focus();
-	// originalCarLot.resetAll();
-	originalCarLot.colorChange(event);
-	originalCarLot.borderChange(event);
+	
+	
 	
 		});
 	}
 };
 
 	// This function is the one that changes the color of each card.
-	originalCarLot.colorChange = function(event) {
-		for(var i = 0; i < cars.length; i++){		
-		let allCars = document.getElementsByClassName(`car${i}`);
-	}	
+	originalCarLot.colorChange = function(Car, ClassName) {		
+			Car.classList.add(ClassName);		
 }
+
 	// This function is the one that changes that increases the size of the border for each card.
-	originalCarLot.borderChange = function(event) {
-		for(var i = 0; i < cars.length; i++){
-		let allCars = document.getElementsByClassName(`car${i}`)
-	};
-}	
-			// This function is supposed to reset all cards back to their original borders and colors when a different one is clicked.
-	// 		originalCarLot.resetAll = function () {
-	// 			if(event.target.parentNode.id === firstCar.id || event.target.id === firstCar.id ){
-	// 			firstCar.classList.remove("firstwidth", "firstbackground")
-	// }
-	// 	else if(event.target.parentNode.id === secondCar.id || event.target.id === secondCar.id){
-	// 			secondCar.classList.remove("secondwidth", "secondbackground")
-	// 	}
-	// 		else if (event.target.parentNode.id === thirdCar.id || event.target.id === thirdCar.id) {
-	// 			thirdCar.classList.remove("thirdwidth", "thirdbackground")
-	// 		}
-	// 			else if (event.target.parentNode.id === fourthCar.id || event.target.id === fourthCar.id) {
-	// 			fourthCar.classList.remove("fourthwidth", "fourthbackground")
-	// 			};
-	// 		};
+	originalCarLot.borderChange = function(Car, BorderClass) {
+			Car.classList.add(BorderClass);	
+	}
+	
 
 // This is where the iifee gets called.
 CarLot.loadInventory(borderEvents);
